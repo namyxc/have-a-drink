18d5fcb39f04095aa118428dc75db895ffe2ead0
Librecz Bernadett <librecz.bernadett@magnetbank.hu>
2020-06-24 15:46:43 +0200

    initial commit

---
+++ app/src/main/java/com/namyxc/haveadrink/view/OneDrinkActivity.kt
@@ -0,0 +1,13 @@
+package com.namyxc.haveadrink.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.namyxc.haveadrink.R
+
+class OneDrinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_drink)
+    }
+}
